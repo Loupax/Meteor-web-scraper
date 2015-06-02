9c32ada080b57ec1726b12cee46e852c362b7504
Kostas Loupasakis <loupax@gmail.com>
2015-06-02 20:35:35 +0300

    Updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'loupax:web-scraper',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'Package that gives you access to web scraping functionality',
     // URL to the Git repository containing the source code for this package.
